715638e68b2066b14e8c069a240a171640aed2bd
Clint Thurman <backlit@gmail.com>
2016-05-13 17:07:44 -0700

    cleanup - moving styles to scss, fixed scroll position on ui route state change

--- src/main/webapp/app/concierge/incident-radio.directive.js
+++ src/main/webapp/app/concierge/incident-radio.directive.js
@@ -1,5 +1,3 @@
-//ToDo: move inline styles to SCSS
-
 (function() {
     'use strict';
 
@@ -14,15 +12,15 @@
                 targetData: '=',
                 radioData: '='
             },
-            template:   '<div style="display:flex; flex-flow: row;align-items: center;text-align: center;margin-bottom: 30px;">' +
-                            '<div style="flex: 1;margin-right:10px;text-align: left;">{{radioData.label}}</div>' +
-                            '<div style="position:relative;top:-5px;flex: 3;">' +
-                                '<div style="position:relative;display:flex;flex-flow: row;z-index: 1;">' +
-                                    '<div style="flex: 1;font-size:11px;" ng-repeat="radio in radioData.values">' +
-                                        '<div><label style="margin:0;cursor: pointer;">{{radio.label}}<br/><input style="cursor: pointer;" name="{{radioData.id}}" type="radio" ng-model="$parent.targetData" value="{{radio.value}}"></label></div>' +
+            template:   '<div class="drsl-incident-radio">' +
+                            '<div class="drsl-radio-desc">{{radioData.label}}</div>' +
+                            '<div class="drsl-radio-wrapper">' +
+                                '<div class="drsl-radio-set">' +
+                                    '<div class="drsl-radio-item" ng-repeat="radio in radioData.values">' +
+                                        '<div><label>{{radio.label}}<br/><input name="{{radioData.id}}" type="radio" ng-model="$parent.targetData" value="{{radio.value}}"></label></div>' +
                                     '</div>' +
                                 '</div>' +
-                                '<hr style="position:absolute;top:75%;left:0;width:100%;margin: 0px;z-index: 0;" />' +
+                                '<hr />' +
                             '</div>' +
                         '</div>'
             ,
