ebc3afdcf09aa1bf91ff66baf037c0e9b7feb8e6
Clint Thurman <backlit@gmail.com>
2016-05-23 11:25:44 -0700

    integrating concierge mock service data with translations

--- src/main/webapp/app/concierge/incident-radio.directive.js
+++ src/main/webapp/app/concierge/incident-radio.directive.js
@@ -13,11 +13,11 @@
                 radioData: '='
             },
             template:   '<div class="drsl-incident-radio">' +
-                            '<div class="drsl-radio-desc">{{radioData.label}}</div>' +
+                            '<div class="drsl-radio-desc" translate="{{radioData.label}}"></div>' +
                             '<div class="drsl-radio-wrapper">' +
                                 '<div class="drsl-radio-set">' +
                                     '<div class="drsl-radio-item" ng-repeat="radio in radioData.values">' +
-                                        '<div><input name="{{radioData.id}}" id="incidentRadio_{{$id}}" type="radio" ng-model="$parent.targetData" value="{{radio.value}}"><label for="incidentRadio_{{$id}}">{{radio.label}}<br><span></span></label></div>' +
+                                        '<div><input name="{{radioData.id}}" id="incidentRadio_{{$id}}" type="radio" ng-model="$parent.targetData" value="{{radio.value}}"><label for="incidentRadio_{{$id}}">{{radio.label | translate}}<br><span></span></label></div>' +
                                     '</div>' +
                                 '</div>' +
                                 '<hr />' +
--- src/main/webapp/app/concierge/product-details.directive.js
+++ src/main/webapp/app/concierge/product-details.directive.js
@@ -13,22 +13,22 @@
                 complete: '='
             },
             template:   '<div class="well drsl-product-details-wrapper" ng-show="canShowProductDetails()">' +
-                            '<h4>{{product.selectedValue}}</h4>' +
+                            '<h4>{{getSelectedProductsLabel() | translate}}</h4>' +
                             '<div><uib-accordion close-others="true"><uib-accordion-group ng-repeat="rootType in getAllIncidentTypes()" is-open="rootType.open">' +
-                            '<uib-accordion-heading>{{rootType.description}}<br /><span class="drsl-product-header-selection" ng-show="rootType.selectedValue">{{rootType.selectedValue}}</span><i class="pull-right glyphicon" ng-class="{\'glyphicon-ok-sign\': rootType.selectedValue}"></i></uib-accordion-heading>' +
+                            '<uib-accordion-heading>{{rootType.description | translate}}<br /><span class="drsl-product-header-selection" ng-show="rootType.selectedValue">{{getLabelForTypeValue(rootType) | translate}}</span><i class="pull-right glyphicon" ng-class="{\'glyphicon-ok-sign\': rootType.selectedValue}"></i></uib-accordion-heading>' +
                                 '<div class="drsl-product-detail-input" ' +
                                      'ng-repeat="incidentType in rootType.types">' +
                                     '<button ng-hide="incidentType.type" type="button" ' +
                                             'class="btn" ' +
-                                            'ng-class="{\'btn-success\': (incidentType.label == rootType.selectedValue)}" ' +
+                                            'ng-class="{\'btn-success\': (incidentType.value == rootType.selectedValue)}" ' +
                                             'ng-click="setIncidentTypeValue(rootType, incidentType)">' +
-                                        '{{incidentType.label}}' +
+                                        '{{incidentType.label | translate}}' +
                                     '</button>' +
                                     '<div ng-show="incidentType.type && incidentType.type === \'field\'">' +
-                                        '<form ng-submit="setIncidentTypeValue(rootType, incidentType)"><input type="text" class="form-control" ng-model="incidentType.label">' +
+                                        '<form ng-submit="setIncidentTypeValue(rootType, incidentType)"><input type="text" class="form-control" ng-model="incidentType.value">' +
                                         '<button type="submit" ' +
                                                 'class="btn btn-primary" ' +
-                                                '>' +
+                                                'translate="concierge.incidentTypes.version.add">' +
                                             'add' +
                                         '</button></form>' +
                                     '</div>' +
@@ -41,74 +41,26 @@
 
         return directive;
 
-        function linkFunc(scope, element, attrs) {
-            /**
-             *
-             */
-            scope.toggleComplete = function () {
-                if (scope.product.incidentTypeSelections.length > 0) {
-                    scope.complete = !scope.complete;
-                }
-            };
+        function linkFunc(scope) {
 
             /**
-             * Returns an IncidentType object to further refine the product details.
-             * @returns {*}
+             * Returns a list of all incident types for the currently selected product.
+             * @returns {Array} an array of incident types for the selected product.
              */
-            scope.getIncidentTypes = function () {
-                var incidentTypes = [],
-                    incidentType;
-
-                if (!scope.complete) {
-                    // Grab the incidentType array for the selected product
-                    if (scope.product.selectedValue) {
-                        incidentTypes = scope.product.values.filter(function (o) {
-                            return o.value === scope.product.selectedValue
-                        });
-                    }
-
-                    // Pull the next *incomplete* IncidentType for this product
-                    if (incidentTypes.length) {
-                        incidentType = incidentTypes[0].incidentTypes[scope.product.incidentTypeSelections.length];
-                    }
-                }
-
-                return incidentType;
-            };
-
             scope.getAllIncidentTypes = function () {
                 var incidentTypes = [];
 
                 // Grab the incidentType array for the selected product
                 if (scope.product && scope.product.selectedValue) {
-                    incidentTypes = scope.product.values.filter(function (o) {
-                        return o.value === scope.product.selectedValue
-                    })[0].incidentTypes;
+                    incidentTypes = scope.getSelectedProduct().incidentTypes;
                 }
 
                 return incidentTypes;
             };
 
             /**
-             * Removes a given IncidentType object from the incidentTypeSelections array.
-             * @param type an IncidentType object
-             */
-            scope.removeIncidentType = function (type) {
-                var incidentTypesLength = scope.product.incidentTypeSelections.length,
-                    index = scope.product.incidentTypeSelections.indexOf(type);
-
-                // Enable further editing
-                scope.complete = false;
-
-                // Remove any incidentTypeSelections after the index of the provided IncidentType object
-                if (index !== -1) {
-                    scope.product.incidentTypeSelections.splice(index, incidentTypesLength);
-                }
-            };
-
-            /**
              * Returns true if we can show the product details for currently selected product.
-             * @returns {boolean}
+             * @returns {boolean} a boolean for deciding to show/hide the product details/incidents.
              */
             scope.canShowProductDetails = function () {
                 var canShow = false,
@@ -119,26 +71,72 @@
                     canShow = true;
 
                     // Check to see if the currently selected product has incidentTypes
-                    hasIncidentTypes = scope.product.values.filter(function (o) {
-                            return o.value === scope.product.selectedValue;
-                        })[0].incidentTypes.length > 0;
+                    hasIncidentTypes = scope.getSelectedProduct().incidentTypes.length > 0;
                 }
 
                 return canShow && hasIncidentTypes;
             };
 
-            scope.setIncidentTypeValue = function (type, value){
-                if (value.type && value.type === 'field'){
-                    type.selectedValue = 'v. ' +value.label;
+            /**
+             * Sets the given incident type's value.
+             * @param type
+             * @param value
+             */
+            scope.setIncidentTypeValue = function (type, value) {
+                if (value.type && value.type === 'field') {
+                    type.selectedValue = (value.value) ? value.value : '';
+                    value.label = (value.value) ? value.value : '';
                 } else {
-                    type.selectedValue = (type.selectedValue === value.label)? '':value.label;
+                    type.selectedValue = (type.selectedValue === value.value) ? '' : value.value;
                 }
 
                 type.open = false;
+            };
 
-            }
+            /**
+             * Returns the currently selected product.
+             * @returns {{}} the currently selected product object.
+             */
+            scope.getSelectedProduct = function () {
+                var selectedProduct = {};
 
+                if (scope.product && scope.product.selectedValue) {
+                    selectedProduct = scope.product.values.filter(function (o) {
+                        return o.value === scope.product.selectedValue
+                    })[0];
+                }
 
+                return selectedProduct;
+            };
+
+            /**
+             * Returns the currently selected product's label.
+             * @returns {string} the currently selected product's label.
+             */
+            scope.getSelectedProductsLabel = function () {
+                return (scope.product && scope.product.selectedValue) ? scope.getSelectedProduct().label : '';
+            };
+
+            /**
+             * Returns the label for the given type's selectedValue type.
+             * @param type an incident type object.
+             * @returns {string} the label for the given type's selectedValue type.
+             */
+            scope.getLabelForTypeValue = function (type) {
+                var label = '',
+                    selectedType = type.types.filter(function (o) {
+                        return o.value === type.selectedValue
+                    })[0];
+
+                // For version, add a prefix to the label
+                if (selectedType && selectedType.type === 'field') {
+                    label = $translate.instant('concierge.incidentTypes.version.prefix') + ' ' + selectedType.label;
+                } else if (selectedType) {
+                    label = selectedType.label;
+                }
+
+                return label;
+            };
         }
     }
 })();
