7cc6db2b9c274e17ab94f6d27e30718fa3b8e751
Clint Thurman <backlit@gmail.com>
2016-05-26 21:26:32 -0700

    DEB-11 Account Management page

--- src/main/webapp/app/account/settings/settings.controller.js
+++ src/main/webapp/app/account/settings/settings.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -7,13 +7,18 @@
 
     SettingsController.$inject = ['Principal', 'Auth', 'JhiLanguageService', '$translate'];
 
-    function SettingsController (Principal, Auth, JhiLanguageService, $translate) {
+    function SettingsController(Principal, Auth, JhiLanguageService, $translate) {
         var vm = this;
 
         vm.error = null;
         vm.save = save;
         vm.settingsAccount = null;
         vm.success = null;
+        vm.authorizedUsers = {};
+        vm.getAuthorizedUsers = getAuthorizedUsers;
+        vm.addAuthorizedUser = addAuthorizedUser;
+        vm.removeAuthorizedUsers = removeAuthorizedUsers;
+        vm.authorizedUser = '';
 
         /**
          * Store the "settings account" in a separate variable, and not in the shared "account" variable.
@@ -29,26 +34,49 @@
             };
         };
 
-        Principal.identity().then(function(account) {
+        Principal.identity().then(function (account) {
             vm.settingsAccount = copyAccount(account);
         });
 
-        function save () {
-            Auth.updateAccount(vm.settingsAccount).then(function() {
+        function save() {
+            Auth.updateAccount(vm.settingsAccount).then(function () {
                 vm.error = null;
                 vm.success = 'OK';
-                Principal.identity(true).then(function(account) {
+                Principal.identity(true).then(function (account) {
                     vm.settingsAccount = copyAccount(account);
                 });
-                JhiLanguageService.getCurrent().then(function(current) {
+                JhiLanguageService.getCurrent().then(function (current) {
                     if (vm.settingsAccount.langKey !== current) {
                         $translate.use(vm.settingsAccount.langKey);
                     }
                 });
-            }).catch(function() {
+            }).catch(function () {
                 vm.success = null;
                 vm.error = 'ERROR';
             });
+        }
+
+        function addAuthorizedUser(){
+            vm.authorizedUsers[vm.authorizedUser] = vm.authorizedUser;
+            vm.authorizedUser = '';
+        }
+
+        function getAuthorizedUsers() {
+            var user, users = [];
+
+            for (user in vm.authorizedUsers) {
+                if (vm.authorizedUsers.hasOwnProperty(user)){
+                    users.push(user);
+                }
+            }
+
+            users.sort();
+
+            return users;
+        }
+
+        function removeAuthorizedUsers(user) {
+            delete vm.authorizedUsers[user];
         }
     }
 })();
