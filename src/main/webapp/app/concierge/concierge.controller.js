fc710eb4977b9ac5d850573c562f70ea00ae5af4
Damon Kalaboukis <damonkala@yahoo.com>
2016-06-07 14:59:44 -0700

    Service is messed up

--- src/main/webapp/app/concierge/concierge.controller.js
+++ src/main/webapp/app/concierge/concierge.controller.js
@@ -3,9 +3,9 @@
 
     angular
         .module('dorsalApp')
-        .controller('ConciergeController', ConciergeController);
+        .controller('ConciergeController', ['$rootScope', '$scope', '$uibModal', '$location', 'MSG', 'ChatRoomsService', 'ChatService', 'SimpleModalService', 'ConciergeService', ConciergeController]);
 
-    ConciergeController.$inject = ['$scope', '$state', 'LoginService', 'Principal', 'ConciergeService', '$translate', '$http'];
+    ConciergeController.$inject = ['$rootScope', '$scope', '$state', 'LoginService', 'Principal', 'ConciergeService', '$translate', '$http', '$location', 'MSG', 'ChatRoomsService', 'ChatService', 'SimpleModalService', 'toastr'];
 
     function ConciergeController($scope, $state, LoginService, Principal, ConciergeService, $translate, $http) {
         var vm = this;
@@ -38,7 +38,53 @@
          */
         function init(){
             vm.pageTitle = '';
+            vm.person = {
+              firstName: '',
+              lastName: ''
+            };
 
+            vm.roomInfo = {}
+            vm.phrases = [];
+
+            vm.joined = false;
+            vm.message = '';
+
+            vm.notify = function (type, message) {
+              if (type === 'say') {
+                var name = (message.from === vm.person.id) ? vm.person.firstName : 'Concierge';
+
+                $scope.$apply(function() {
+                    vm.phrases.push({message: message.message, username: name});
+                });
+                //ChatRoomsService.formatMessage(message.message, name);
+              }
+              else if (type === 'welcome') {
+                $scope.$apply(function() {
+                    // vm.phrases.push({message: MSG.WelcomeChatGuest1 + vm.person.firstName + MSG.WelcomeChatGuest2, username: 'Administrator'});
+                });
+                //ChatRoomsService.formatMessage(MSG.WelcomeChatGuest1 + vm.person.firstName + MSG.WelcomeChatGuest2, 'Administrator');
+              }
+            }
+            vm.post = function () {
+              ChatService.post(vm.roomInfo.room, vm.message);
+              vm.message = '';
+            }
+            vm.join = function () {
+              ChatService.connect(vm.person, vm.notify, function (error, info) {
+                if (error) {
+                  toastr.error('Failed to connect ' + error, 'Error');
+                  return;
+                }
+                vm.person.id = info.data.id;
+                var roomName = ChatService.makeRoomName(vm.person);
+                ChatService.createChatRoom(roomName, vm.person, function (response) {
+                    vm.joined = true;
+                    vm.roomInfo = response.room;
+                    console.log("RC", $scope.rc.phrases);
+                    //console.log('--- connected ' + JSON.stringify(vm.roomInfo));
+                })
+              })
+            };
             // Make a call to get the initial data.
             ConciergeService.get(function(data){
                 vm.caseDetails = data[0] || [];
