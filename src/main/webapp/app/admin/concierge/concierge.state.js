f9e3d4bd1206ae2ba048f834fc71f61340606e0b
Damon Kalaboukis <damonkala@yahoo.com>
2016-06-07 15:57:41 -0700

    DEB-19 Added chatrooms for admins on concierge side

---
+++ src/main/webapp/app/admin/concierge/concierge.state.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('dorsalApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('conciergeAdmin', {
+            parent: 'admin',
+            url: '/conciergeAdmin',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'concierge.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/admin/concierge/conciergeAdmin.html'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', function ($translate) {
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
