394a94a5ab755acc32603257ad329f87bdbb59bd
Clint Thurman <backlit@gmail.com>
2016-06-09 17:00:27 -0700

    DEB-12 Case overview page. Ratings updates.

--- src/main/webapp/app/rating/rating.controller.js
+++ src/main/webapp/app/rating/rating.controller.js
@@ -13,6 +13,59 @@
         vm.cancel = cancel;
         vm.submit = submit;
 
+        vm.badges = [
+            {
+                "icon": "badge-1",
+                "description": "happy fun place"
+            },
+            {
+                "icon": "badge-2",
+                "description": "defender of the realm"
+            },
+            {
+                "icon": "badge-3",
+                "description": "craftsperson"
+            },
+            {
+                "icon": "badge-4",
+                "description": "detail oriented"
+            },
+            {
+                "icon": "badge-5",
+                "description": "focused like a laser"
+            },
+            {
+                "icon": "badge-6",
+                "description": "McLuvin It!"
+            },
+            {
+                "icon": "badge-7",
+                "description": "masterful multitasking"
+            },{
+                "icon": "badge-8",
+                "description": "network specialist"
+            },{
+                "icon": "badge-9",
+                "description": "double plus good"
+            },
+            {
+                "icon": "badge-10",
+                "description": "database magician"
+            },
+            {
+                "icon": "badge-11",
+                "description": "super heavy lifter"
+            },
+            {
+                "icon": "badge-12",
+                "description": "resolution writer pro"
+            },
+            {
+                "icon": "badge-13",
+                "description": "time sensitive"
+            }
+        ];
+
         vm.radios = [
             {
                 "id": "satisfaction",
