73fd03291ffd38f5504b2660a3bb2dee98dcb600
Damon Kalaboukis <damonkala@yahoo.com>
2016-07-14 10:06:28 -0700

    The file upload template is now inline the file-upload.directive.js file

--- src/main/webapp/app/components/form/file-upload.directive.js
+++ src/main/webapp/app/components/form/file-upload.directive.js
@@ -13,6 +13,7 @@
                 result.reverse().forEach(function(attachment){
                     if(attachment.supportcase.id === $scope.currentcase){
                         $scope.attachments.push(attachment)
+                        console.log($scope.attachments);
                     }
                 })
             })
@@ -38,7 +39,22 @@
                 currentcase: '='
             },
             controller: controller,
-            templateUrl: '/app/components/form/file-upload.html'
+            template: '<div ng-repeat="attachment in attachments" style="padding-bottom: 20px; display: inline-block;">{{attachment.name}}' +
+            '<br>' +
+            '{{attachment.dataStreamContentType}}' +
+            '<a class="pull-left" ng-click="openFile(attachment.dataStreamContentType, attachment.dataStream)">' +
+            '<img data-ng-src="data:image/png;base64,{{attachment.dataStream}}" alt="" style="width: 100px;" ng-if="attachment.dataStreamContentType.split(\'/\')[0] == \'image\'">' +
+            '<img src="http://neowin.s3.amazonaws.com/forum/uploads/monthly_04_2013/post-360412-0-09676400-1365986245.png" style="width: 100px;" alt="" ng-if="attachment.dataStreamContentType.split(\'/\')[0] !== \'image\' && attachment.dataStreamContentType">'
+            + '</a>'
+            + '</div>'
+            + '<div class="drsl-file-upload-component" class="form-group">'
+            + '{{attachment.name}}'
+            + '<img data-ng-src="data:image/png;base64,{{attachment.dataStream}}" alt="" style="width: 100px;" ng-if="attachment.dataStreamContentType.split(\'/\')[0] == \'image\'">'
+            + '<img src="http://neowin.s3.amazonaws.com/forum/uploads/monthly_04_2013/post-360412-0-09676400-1365986245.png" style="width: 100px;" alt="" ng-if="attachment.dataStreamContentType.split(\'/\')[0] !== \'image\' && attachment.dataStreamContentType">'
+            + '<i class="fa fa-cloud-upload fa-lg"></i>'
+            + '{{"global.form.fileupload.drop" | translate}}'
+            + '<span type="file" ngf-select ngf-change="setDataStream($file, attachment)">{{"global.form.fileupload.browse" | translate}}</span>'
+            + '</div>'
         };
 
         return directive;
