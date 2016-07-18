17ceb368065b286118b20c02a92933a486c851f8
Damon Kalaboukis <damonkala@yahoo.com>
2016-07-18 12:20:47 -0700

    DEB-106 Change file upload layout

--- src/main/webapp/app/components/form/file-upload.directive.js
+++ src/main/webapp/app/components/form/file-upload.directive.js
@@ -48,21 +48,23 @@
                 currentcase: '='
             },
             controller: controller,
-            template: '<div style="overflow-y: scroll; max-height: 318px;">' + '<div ng-repeat="attachment in attachments track by $index" style="display: inline-block;" class="attachmentItem">' +
-            '<div uib-tooltip="{{attachment.name}}">{{attachment.name.substring(0, 10)}}' + '<span ng-if="attachment.name.length > 10">...</span>' + '</div>' +
-            '<a ng-click="openFile(attachment.dataStreamContentType, attachment.dataStream)" style="font-size: 20px;">' +
+            template: '<div style="overflow-y: scroll; max-height: 58px;">' + '<li ng-repeat="attachment in attachments track by $index" style="outline: none; display: block;" class="attachmentItem" ng-click="options = !options">' +
+            '<div style="display: inline-block;">{{attachment.name}}' + '</div>' +
+            '<div style="display: inline-block; margin-left: 10px;" ng-if="options">' +
+            // '<div uib-tooltip="{{attachment.name}}" style="display: inline-block;">{{attachment.name.substring(0, 10)}}' + '<span ng-if="attachment.name.length > 10">...</span>' + '</div>' + '<div style="display: inline-block;" ng-if="options">' +
+            '<a ng-click="openFile(attachment.dataStreamContentType, attachment.dataStream)" style="font-size: 20px; margin-right: 7px;">' +
             '<i class="fa fa-search fa-lg" aria-hidden="true"></i>' +
             '</a>' +
-            '<a href="data:application/octet-stream;charset=utf-16le;base64,{{attachment.dataStream}}" download="{{attachment.name}}" style="font-size: 20px;">' +
+            '<a href="data:application/octet-stream;charset=utf-16le;base64,{{attachment.dataStream}}" download="{{attachment.name}}" style="font-size: 20px; margin-right: 7px;">' +
             '<i class="fa fa-download fa-lg" aria-hidden="true"></i>' +
             '</a>' +
-            '<a ng-click="deleteAttachment(attachment, $index)" style="font-size: 25px;">' +
+            '<a ng-click="deleteAttachment(attachment, $index)" style="font-size: 25px; margin-right: 7px;">' +
             '<i class="fa fa-times-circle" aria-hidden="true"></i>' +
             '</a>' +
-            '<br>' +
-            '<img data-ng-src="data:image/png;base64,{{attachment.dataStream}}" alt="" style="width: 75px;max-height: 50px;" ng-if="attachment.dataStreamContentType.split(\'/\')[0] == \'image\'">' +
-            '<img src="http://neowin.s3.amazonaws.com/forum/uploads/monthly_04_2013/post-360412-0-09676400-1365986245.png" style="width: 50px; height: 45px;" alt="" ng-if="attachment.dataStreamContentType.split(\'/\')[0] !== \'image\' && attachment.dataStreamContentType">'
-            + '</div>'
+            '</div>' +
+            // '<img data-ng-src="data:image/png;base64,{{attachment.dataStream}}" alt="" style="width: 75px;max-height: 50px;" ng-if="attachment.dataStreamContentType.split(\'/\')[0] == \'image\'">' +
+            // '<img src="http://neowin.s3.amazonaws.com/forum/uploads/monthly_04_2013/post-360412-0-09676400-1365986245.png" style="width: 50px; height: 45px;" alt="" ng-if="attachment.dataStreamContentType.split(\'/\')[0] !== \'image\' && attachment.dataStreamContentType">'
+            '</li>'
             + '</div>'
             + '<div class="drsl-file-upload-component" class="form-group">'
             + '{{attachment.name}}'
