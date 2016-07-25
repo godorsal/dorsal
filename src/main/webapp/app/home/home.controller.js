165bc9cc0902d509ce0f0d25dcdc1df0fc1680c5
Damon Kalaboukis <damonkala@yahoo.com>
2016-07-25 10:03:44 -0700

    DEB-145 Prevents JavaScript Error on landing page

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -25,7 +25,7 @@
             Principal.identity().then(function(account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
-                if(!account.firstName.length && !account.lastName.length){
+                if(account && !account.firstName.length && !account.lastName.length){
                     $state.go('settings')
                 } else if(account){
                     $state.go('case')
