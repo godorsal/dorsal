841886aa3c9ff4cf691850968b149c2ce9ed5131
Damon Kalaboukis <damon.kalaboukis@godorsal.com>
2016-07-26 21:32:55 -0700

    DEB-181 Shelf and tech props added to escalation case

--- src/main/webapp/app/escalation-form/escalation-form.controller.js
+++ src/main/webapp/app/escalation-form/escalation-form.controller.js
@@ -5,14 +5,15 @@
     .module('dorsalApp')
     .controller('EscalationFormController', EscalationFormController);
 
-    EscalationFormController.$inject = ['$rootScope', '$state', '$timeout', 'Auth', '$uibModalInstance', '$translate', 'drslCase', 'expert', 'EscalateCase'];
+    EscalationFormController.$inject = ['$rootScope', '$state', '$timeout', 'Auth', '$uibModalInstance', '$translate', 'drslCase', 'expert', 'EscalateCase', 'Casetechnologyproperty'];
 
-    function EscalationFormController($rootScope, $state, $timeout, Auth, $uibModalInstance, $translate, drslCase, expert, EscalateCase) {
+    function EscalationFormController($rootScope, $state, $timeout, Auth, $uibModalInstance, $translate, drslCase, expert, EscalateCase, Casetechnologyproperty) {
         var vm = this;
         vm.cancel = cancel;
         vm.submit = submit;
         vm.case = drslCase;
         vm.expert = expert;
+        vm.technologyProps = [];
         vm.summary = vm.case.summary.toString();
 
         function cancel(e) {
@@ -24,6 +25,13 @@
                 return escalation.supportcase.id == vm.case.id;
             })
         })
+        Casetechnologyproperty.query(function(result) {
+            result.forEach(function(property){
+                if(property.supportcase.id === vm.case.id){
+                    vm.technologyProps.push(property)
+                }
+            })
+        });
         function submit() {
             vm.escalation = vm.thisEscalation;
             vm.escalation.escalationType = vm.thisEscalation.escalationType
