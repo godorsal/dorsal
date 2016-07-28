6548bac6f67c6f742471366ff6b2a2965ceb2599
Damon Kalaboukis <damon.kalaboukis@godorsal.com>
2016-07-28 14:58:01 -0700

    DEB-154 Activation page orange with white text added error page styling and brought back functionality removed for testing

--- src/main/webapp/app/account/activate/activate.controller.js
+++ src/main/webapp/app/account/activate/activate.controller.js
@@ -9,15 +9,14 @@
 
     function ActivationController ($stateParams, Auth, LoginService) {
         var vm = this;
-            vm.success = 'OK';
 
-        // Auth.activateAccount({key: $stateParams.key}).then(function () {
-        //     vm.error = null;
-        //     vm.success = 'OK';
-        // }).catch(function () {
-        //     vm.success = null;
-        //     vm.error = 'ERROR';
-        // });
+        Auth.activateAccount({key: $stateParams.key}).then(function () {
+            vm.error = null;
+            vm.success = 'OK';
+        }).catch(function () {
+            vm.success = null;
+            vm.error = 'ERROR';
+        });
 
         vm.login = LoginService.open;
     }
