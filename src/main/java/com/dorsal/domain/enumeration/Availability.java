a3397c1c51846883077f26682d307336112c9570
rogerrut <roger.ruttimann@godorsal.com>
2016-08-01 11:16:07 -0700

    JIRA DEB-212
    Updated the following entities: Useraccount, ExpertAccount and Supportcase
    
    Notes:
    --> ExpertAccount has now FK to Technology preferences (firstTechnology, secondTechnology, issueExpertise)
    --> Time zone can now be stored with expert record
    --> Supportcase includes expectedCompletionDate, expertMessage and nbOfUpdates fields
    
    The datamodel requirements came after workflow review with Experts

---
+++ src/main/java/com/dorsal/domain/enumeration/Availability.java
@@ -0,0 +1,8 @@
+package com.dorsal.domain.enumeration;
+
+/**
+ * The Availability enumeration.
+ */
+public enum Availability {
+    FULL_TIME,MON_FRI,EVENING,WEEKEND
+}
---
+++ src/main/java/com/dorsal/domain/enumeration/ProfileVisability.java
@@ -0,0 +1,8 @@
+package com.dorsal.domain.enumeration;
+
+/**
+ * The ProfileVisability enumeration.
+ */
+public enum ProfileVisability {
+    PUBLIC,INTERN,HIDDEN
+}
