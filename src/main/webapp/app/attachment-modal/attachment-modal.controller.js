f2552a5ade4c29e77c7366209a00e63d35b77439
Clint Thurman <clint.thurman@godorsal.com>
2016-08-10 12:52:38 -0700

    DEB-269 Attach function upgrade. Attachment dialog cleanup.

--- src/main/webapp/app/attachment-modal/attachment-modal.controller.js
+++ src/main/webapp/app/attachment-modal/attachment-modal.controller.js
@@ -5,49 +5,13 @@
     .module('dorsalApp')
     .controller('AttachmentModalController', AttachmentModalController);
 
-    AttachmentModalController.$inject = ['$rootScope', '$scope', '$state', '$timeout', 'Auth', '$uibModalInstance', '$translate', 'drslCase', 'expert', 'User', 'Register', 'Principal', 'Attachment', 'DrslAttachFileService'];
+    AttachmentModalController.$inject = ['$scope', '$timeout', '$uibModalInstance', 'drslCase', 'DrslAttachFileService'];
 
-    function AttachmentModalController($rootScope, $scope, $state, $timeout, Auth, $uibModalInstance, $translate, drslCase, expert, User, Register, Principal, Attachment, DrslAttachFileService) {
+    function AttachmentModalController($scope, $timeout, $uibModalInstance, drslCase, DrslAttachFileService) {
         var vm = this;
-        vm.cancel = cancel;
         vm.case = drslCase;
-        vm.submit = submit;
-        vm.attachment = {
-            name: null,
-            url: null,
-            dataStream: null,
-            dataStreamContentType: null,
-            id: null
-        };
-        vm.modalType = "attachment";
-        vm.savingFile = false;
-        $timeout(function(){$scope.$broadcast('currentCaseSet')}, 1);
 
-        function submit() {
-            if(vm.attachment.dataStream && vm.savingFile == false){
-                $rootScope.$broadcast("savingFile")
-                vm.savingFile = true;
-                vm.attachment.supportcase = {
-                    id: vm.case.id
-                }
-                Attachment.save(vm.attachment, onSaveSuccess, onSaveError);
-            }
-        }
-        function onSaveSuccess(data){
-            $rootScope.$broadcast("fileuploaded")
-            vm.savingFile = false;
-        }
-        function onSaveError(error){
-            console.log(error);
-        }
-        function cancel(e) {
-            $rootScope.$broadcast("doneUploading");
-            e.preventDefault();
-            $uibModalInstance.dismiss('cancel');
-        }
-        Principal.identity().then(function (account) {
-            vm.currentUser = account;
-        });
+        $timeout(function(){$scope.$broadcast('currentCaseSet')}, 1);
 
         $scope.$on('doneWithAttachments', function () {
             DrslAttachFileService.uploadAttachFileList(vm.case);
