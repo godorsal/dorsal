928c939b0e435bf6b5ec0aeadfc1c17f239f69e7
Damon Kalaboukis <damon.kalaboukis@godorsal.com>
2016-08-17 11:35:42 -0700

    Restore YML file and fix indentation

--- src/main/webapp/app/case/contact-info.directive.js
+++ src/main/webapp/app/case/contact-info.directive.js
@@ -13,27 +13,27 @@
                 chatRoom: '='
             },
             template:   '<div class="drsl-contact-info">' +
-            '<div ng-show="contact.user.email">' +
-            '<i class="fa fa-envelope" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.email\' | translate}}" ></i> ' +
-            '{{contact.user.email}}' +
-            '</div>' +
-            '<div ng-show="contact.phone">' +
-            '<i class="fa fa-phone" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.phone\' | translate}}"></i> ' +
-            '{{contact.phone}}' +
-            '</div>' +
-            '<div ng-show="contact.skype">' +
-            '<i class="fa fa-skype" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.skype\' | translate}}"></i> ' +
-            '{{contact.skype}}' +
-            '</div>' +
-            '<div ng-show="chatRoom">' +
-            '<i class="fa fa-comments-o" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.chat\' | translate}}"></i> ' +
-            '<a ng-href="{{chatRoom.link}}" target="_blank">{{chatRoom.id}}</a>' +
-            '</div>' +
-            '<div>' +
-            '<i class="fa fa-comments-o" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.other\' | translate}}"></i> ' +
-            '<a ng-href="{{otherTitle}}" target="_blank">{{otherLink}}</a>' +
-            '</div>' +
-            '</div>'
+                            '<div ng-show="contact.user.email">' +
+                            '<i class="fa fa-envelope" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.email\' | translate}}" ></i> ' +
+                            '{{contact.user.email}}' +
+                        '</div>' +
+                        '<div ng-show="contact.phone">' +
+                            '<i class="fa fa-phone" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.phone\' | translate}}"></i> ' +
+                            '{{contact.phone}}' +
+                        '</div>' +
+                        '<div ng-show="contact.skype">' +
+                            '<i class="fa fa-skype" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.skype\' | translate}}"></i> ' +
+                            '{{contact.skype}}' +
+                        '</div>' +
+                        '<div ng-show="chatRoom">' +
+                            '<i class="fa fa-comments-o" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.chat\' | translate}}"></i> ' +
+                            '<a ng-href="{{chatRoom.link}}" target="_blank">{{chatRoom.id}}</a>' +
+                        '</div>' +
+                        '<div>' +
+                            '<i class="fa fa-comments-o" tooltip-placement="left" uib-tooltip="{{\'case.professional.contact.other\' | translate}}"></i> ' +
+                            '<a ng-href="{{otherTitle}}" target="_blank">{{otherLink}}</a>' +
+                        '</div>' +
+                    '</div>'
             ,
             link: linkFunc,
             controller: scopeController
