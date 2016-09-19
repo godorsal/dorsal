389e1fb7ecaeba6462acf28e7b14885cf55dd03c
Damon Kalaboukis <damon.kalaboukis@godorsal.com>
2016-09-19 11:18:08 -0700

    Support case reports

--- src/main/java/com/dorsal/web/rest/SupportcaseResource.java
+++ src/main/java/com/dorsal/web/rest/SupportcaseResource.java
@@ -214,13 +214,14 @@ public class SupportcaseResource {
     public List<Supportcase> getAllSupportcases() {
         log.debug("REST request to get all Supportcases");
         int numCases = 0;
+        
         // Get all support cases if logged in user is admin
         List<Supportcase> supportcases = supportcaseRepository.findAllAdminIsCurrentUser();
 
         if(supportcases.size() > 0) {
             return supportcases;
         }
-        
+
         //.findAll();
         // List<Supportcase> supportcases = supportcaseRepository.findByUserIsCurrentUser(); //.findAll();
         // List<Supportcase> supportcases = supportcaseRepository.findByUserIsCurrentUser(); //.findAll();
