a8a51c8251b334213fcc53ae3ed83f0a721f3562
Damon Kalaboukis <damon.kalaboukis@godorsal.com>
2016-09-23 16:22:49 -0700

    DEB-3: JAVA  Removed Transactional from SupportCaseResource.jaba

--- src/main/java/com/dorsal/web/rest/SupportcaseResource.java
+++ src/main/java/com/dorsal/web/rest/SupportcaseResource.java
@@ -29,7 +29,6 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 
 
 /**
@@ -216,7 +215,6 @@ public class SupportcaseResource {
          method = RequestMethod.GET,
          produces = MediaType.APPLICATION_JSON_VALUE)
      @Timed
-     @Transactional(readOnly = true)
      public ResponseEntity<List<Supportcase>> getAllSupportcases(Pageable pageable)
          throws URISyntaxException {
              log.debug("REST request to get all Supportcases");
