545a90d63455ecf4a6e72011e40e00e14e335005
rogerrut <roger.ruttimann@godorsal.com>
2016-11-17 15:03:00 -0800

    JIRA: DEB-399
    Ordering of supportcases was wrong for paging mode. Changed it from ASC default to DESC

--- src/main/java/com/dorsal/repository/SupportcaseRepository.java
+++ src/main/java/com/dorsal/repository/SupportcaseRepository.java
@@ -14,29 +14,29 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface SupportcaseRepository extends JpaRepository<Supportcase,Long> {
 
-    @Query("select supportcase from Supportcase supportcase where supportcase.user.login = ?#{principal.username} ORDER BY supportcase.dateCreated ASC")
+    @Query("select supportcase from Supportcase supportcase where supportcase.user.login = ?#{principal.username} ORDER BY supportcase.dateCreated DESC")
     List<Supportcase> findByUserIsCurrentUser();
 
-    @Query(value = "select supportcase from Supportcase supportcase where supportcase.expertaccount.user.login = ?#{principal.username}  ORDER BY supportcase.dateCreated ASC",
+    @Query(value = "select supportcase from Supportcase supportcase where supportcase.expertaccount.user.login = ?#{principal.username}  ORDER BY supportcase.dateCreated DESC",
            countQuery = "select count(supportcase.id) from Supportcase supportcase where supportcase.expertaccount.user.login = ?#{principal.username}")
     Page<Supportcase> findByExpertIsCurrentUser(Pageable pageable);
 
-    @Query(value = "select supportcase from Supportcase supportcase where ?#{principal.username} = 'admin'  ORDER BY supportcase.dateCreated ASC",
+    @Query(value = "select supportcase from Supportcase supportcase where ?#{principal.username} = 'admin'  ORDER BY supportcase.dateCreated DESC",
            countQuery = "select count(supportcase) from Supportcase supportcase where ?#{principal.username} = 'admin' ")
     Page<Supportcase>  findAllAdminIsCurrentUser(Pageable pageable);
     //List<Supportcase> findAllAdminIsCurrentUser();
 
-    @Query(value = "select supportcase from Supportcase supportcase where supportcase.user.login = ?#{principal.username} ORDER BY supportcase.dateCreated ASC",
+    @Query(value = "select supportcase from Supportcase supportcase where supportcase.user.login = ?#{principal.username} ORDER BY supportcase.dateCreated DESC",
            countQuery = "select count(supportcase.id) from Supportcase supportcase where supportcase.user.login = ?#{principal.username}")
     Page<Supportcase> findByOwnerIsCurrentUser(Pageable pageable);
 
-    @Query("select supportcase from Supportcase supportcase, SharedCase sharedcase where (supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username})  ORDER BY supportcase.dateCreated ASC")
+    @Query("select supportcase from Supportcase supportcase, SharedCase sharedcase where (supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username})  ORDER BY supportcase.dateCreated DESC")
     List<Supportcase> findBySharedIsCurrentUser();
 
-    @Query("select supportcase from Supportcase supportcase, Groupaccess groupaccess where (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username})  ORDER BY supportcase.dateCreated ASC")
+    @Query("select supportcase from Supportcase supportcase, Groupaccess groupaccess where (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username})  ORDER BY supportcase.dateCreated DESC")
     List<Supportcase> findGroupAccessUser();
 
-    @Query(value ="select supportcase from Supportcase supportcase, SharedCase sharedcase, Groupaccess groupaccess where ((supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username}) AND (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username}))  ORDER BY supportcase.dateCreated ASC" ,
+    @Query(value ="select supportcase from Supportcase supportcase, SharedCase sharedcase, Groupaccess groupaccess where ((supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username}) AND (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username}))  ORDER BY supportcase.dateCreated DESC" ,
         countQuery = "select count(supportcase.id) from Supportcase supportcase, SharedCase sharedcase, Groupaccess groupaccess where ((supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username}) AND (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username})) ")
     Page<Supportcase>findIsSharedwithCurrentUser(Pageable pageable);
 }
