77550d101667b0bf81da8431613f93ef266e8843
rogerrut <roger.ruttimann@godorsal.com>
2016-11-16 20:17:04 -0800

    JIRA: DEB-399
    support case get is adapted for pagination. Queries and subqueries are getting paged.
    
    Added specific API's for owner, expert and shared cases by logged in  user

--- src/main/java/com/dorsal/repository/SupportcaseRepository.java
+++ src/main/java/com/dorsal/repository/SupportcaseRepository.java
@@ -17,17 +17,26 @@ public interface SupportcaseRepository extends JpaRepository<Supportcase,Long> {
     @Query("select supportcase from Supportcase supportcase where supportcase.user.login = ?#{principal.username} ORDER BY supportcase.dateCreated ASC")
     List<Supportcase> findByUserIsCurrentUser();
 
-    @Query("select supportcase from Supportcase supportcase where supportcase.expertaccount.user.login = ?#{principal.username}  ORDER BY supportcase.dateCreated ASC")
-    List<Supportcase> findByExpertIsCurrentUser();
+    @Query(value = "select supportcase from Supportcase supportcase where supportcase.expertaccount.user.login = ?#{principal.username}  ORDER BY supportcase.dateCreated ASC",
+           countQuery = "select count(supportcase.id) from Supportcase supportcase where supportcase.expertaccount.user.login = ?#{principal.username}")
+    Page<Supportcase> findByExpertIsCurrentUser(Pageable pageable);
 
     @Query(value = "select supportcase from Supportcase supportcase where ?#{principal.username} = 'admin'  ORDER BY supportcase.dateCreated ASC",
            countQuery = "select count(supportcase) from Supportcase supportcase where ?#{principal.username} = 'admin' ")
     Page<Supportcase>  findAllAdminIsCurrentUser(Pageable pageable);
     //List<Supportcase> findAllAdminIsCurrentUser();
 
+    @Query(value = "select supportcase from Supportcase supportcase where supportcase.user.login = ?#{principal.username} ORDER BY supportcase.dateCreated ASC",
+           countQuery = "select count(supportcase.id) from Supportcase supportcase where supportcase.user.login = ?#{principal.username}")
+    Page<Supportcase> findByOwnerIsCurrentUser(Pageable pageable);
+
     @Query("select supportcase from Supportcase supportcase, SharedCase sharedcase where (supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username})  ORDER BY supportcase.dateCreated ASC")
     List<Supportcase> findBySharedIsCurrentUser();
 
     @Query("select supportcase from Supportcase supportcase, Groupaccess groupaccess where (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username})  ORDER BY supportcase.dateCreated ASC")
     List<Supportcase> findGroupAccessUser();
+
+    @Query(value ="select supportcase from Supportcase supportcase, SharedCase sharedcase, Groupaccess groupaccess where ((supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username}) AND (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username}))  ORDER BY supportcase.dateCreated ASC" ,
+        countQuery = "select count(supportcase.id) from Supportcase supportcase, SharedCase sharedcase, Groupaccess groupaccess where ((supportcase.id = sharedcase.supportcase.id) AND (sharedcase.user.login = ?#{principal.username}) AND (supportcase.user.id = groupaccess.authorizeduser.id) AND (groupaccess.user.login = ?#{principal.username})) ")
+    Page<Supportcase>findIsSharedwithCurrentUser(Pageable pageable);
 }
